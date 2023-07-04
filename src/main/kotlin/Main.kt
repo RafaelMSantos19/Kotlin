1aabdce7075a17c3e18ea2c03c50a3840cc69a1d
Rafael Santos Sysdata <rafael.santos@sysdata.com>
2023-07-04 18:29:37 -0300

    Variaveis do tipo numerico

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,19 @@
 fun main() {
 
-    var palavras: String
-    var numeros: Int
+    // Tipo de variaveis numericas:
+    println("Double Max Value: ${Double.MAX_VALUE} \n Double Min Value: ${Double.MIN_VALUE}")
+    println("Float Max Value: ${Float.MAX_VALUE} \n Float Min Value: ${Float.MIN_VALUE}")
+    println("Int Max Value: ${Int.MAX_VALUE} \n Int Min Value: ${Int.MIN_VALUE}")
+    println("Long Max Value: ${Long.MAX_VALUE} \n Long Min Value: ${Long.MIN_VALUE}")
+    println("Short Max Value: ${Short.MAX_VALUE} \n Short Min Value: ${Short.MIN_VALUE}")
+    println("Byte Max Value: ${Byte.MAX_VALUE} \n Byte Min Value: ${Byte.MIN_VALUE}")
 
-    var nome = "Rafael Santos"
-
-    println(nome)
+    var double: Double = 10.1
+    var float: Float = 10f
+    var long: Long = 10L
+    var int: Int = 10
+    var short: Short = 10
+    var byte: Byte = 10
 }
 
 
