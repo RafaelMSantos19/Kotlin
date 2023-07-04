ae4700e4d01ba5a4844b361dc1262c60740812e6
Rafael Santos <rafarag40@gmail.com>
2023-07-04 20:22:22 -0300

    Variaveis Unsiged

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,20 @@
 fun main() {
 
-    // Tipo de variaveis numericas:
-    println("Double Max Value: ${Double.MAX_VALUE} \n Double Min Value: ${Double.MIN_VALUE}")
-    println("Float Max Value: ${Float.MAX_VALUE} \n Float Min Value: ${Float.MIN_VALUE}")
-    println("Int Max Value: ${Int.MAX_VALUE} \n Int Min Value: ${Int.MIN_VALUE}")
-    println("Long Max Value: ${Long.MAX_VALUE} \n Long Min Value: ${Long.MIN_VALUE}")
-    println("Short Max Value: ${Short.MAX_VALUE} \n Short Min Value: ${Short.MIN_VALUE}")
-    println("Byte Max Value: ${Byte.MAX_VALUE} \n Byte Min Value: ${Byte.MIN_VALUE}")
+    // Tipo de variaveis Unsiged:
 
-    var double: Double = 10.1
-    var float: Float = 10f
-    var long: Long = 10L
-    var int: Int = 10
-    var short: Short = 10
-    var byte: Byte = 10
+    println("Unt Max Value: ${UInt.MAX_VALUE} \n UInt Min Value: ${UInt.MIN_VALUE}")
+    println("ULong Max Value: ${ULong.MAX_VALUE} \n ULong Min Value: ${ULong.MIN_VALUE}")
+    println("UShort Max Value: ${UShort.MAX_VALUE} \n UShort Min Value: ${UShort.MIN_VALUE}")
+    println("UByte Max Value: ${UByte.MAX_VALUE} \n UByte Min Value: ${UByte.MIN_VALUE}")
+
+    val uint: UInt = 10u
+
+    val ushort: UShort = 10u
+
+    val ubyte: UByte =10u
+
+    val ulong: ULong =10u
+
 }
 
 
