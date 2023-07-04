cdf42cfc3dd2e5f7037abc93e94c71fb3dcd3f18
Rafael Santos <rafarag40@gmail.com>
2023-07-04 00:36:52 -0300

    Como cria varios tipos de comentarios em kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,14 @@
 fun main() {
     println("Ola mundo!")
 }
+
+// Isso e um comentario dentro do codigo em kotlin
+
+/* comentario
+ de
+ varias linhas
+ */
+
+/**
+ * Comentario de documentacao
+ */
