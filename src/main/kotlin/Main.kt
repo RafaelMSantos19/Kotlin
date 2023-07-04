0c33023f4c22445912c059d1750d6edb52ee24e7
Rafael Santos <rafarag40@gmail.com>
2023-07-04 20:44:19 -0300

    operacoes matematicas

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,26 @@
 fun main() {
 
-    val nome = "Rafael"
-    val sobrenome = " Santos"
+    var idade = 30
 
-    //concatenar
-    val nomeCompleto = nome+sobrenome
+    var idadeSum = idade +1
+    var idadeSub = idade -1
+    var idadeMult = idade * 2
+    var idadeDiv = idade / 2
 
-    println(nomeCompleto)
+    println("Idade total: $idade")
+    println("Idade somada: $idadeSum")
+    println("Idade subitraida: $idadeSub")
+    println("Idade Multiplicada: $idadeMult")
+    println("Idade Dividida: $idadeDiv")
 
-    //interpolar
-    println("Meu nome e $nomeCompleto")
+    var resto = idade % 2
 
+    println("Resto da divisao: $resto")
+
+    // Outras formas de soma variaveis
+    println(idade++)
+    println(idade)
+    println(++idade)
 
 }
 
