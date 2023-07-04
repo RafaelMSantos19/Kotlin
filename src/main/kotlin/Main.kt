08512836f6a46251b333372cf8f674c03704670a
Rafael Santos <rafarag40@gmail.com>
2023-07-04 20:25:25 -0300

    variaveis constantes e variantes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,15 @@
 fun main() {
 
-    // Tipo de variaveis Unsiged:
+    // variaveu que pode mudar a o longo do codigo
+    var nome = "Tony"
+    nome ="Stark"
 
-    println("Unt Max Value: ${UInt.MAX_VALUE} \n UInt Min Value: ${UInt.MIN_VALUE}")
-    println("ULong Max Value: ${ULong.MAX_VALUE} \n ULong Min Value: ${ULong.MIN_VALUE}")
-    println("UShort Max Value: ${UShort.MAX_VALUE} \n UShort Min Value: ${UShort.MIN_VALUE}")
-    println("UByte Max Value: ${UByte.MAX_VALUE} \n UByte Min Value: ${UByte.MIN_VALUE}")
+    println(nome)
 
-    val uint: UInt = 10u
+    // variaveu constante e imutaveu
+    val nome2 = "Rafael"
 
-    val ushort: UShort = 10u
-
-    val ubyte: UByte =10u
-
-    val ulong: ULong =10u
-
+    println(nome2)
 }
 
 
