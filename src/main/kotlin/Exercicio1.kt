55f573c34116428ef6b26fd5621df434ea5d282b
Rafael Santos <rafarag40@gmail.com>
2023-07-04 21:18:41 -0300

    Exercicio 1 Comcluido

---
+++ src/main/kotlin/Exercicio1.kt
@@ -0,0 +1,15 @@
+fun exercicio(){
+    //Respostas
+    var nomeCompleto = "Rafael Santos"
+
+    var string: String
+
+    val TamanhoDoCalcado: Byte = 42
+
+    val PIB:Long = 1869000000000
+
+    val Populacao:Int = 211000000
+
+    println("Pib per capita = ${PIB/Populacao}")
+}
+
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,27 +1,5 @@
 fun main() {
 
-    var idade = 30
-
-    var idadeSum = idade +1
-    var idadeSub = idade -1
-    var idadeMult = idade * 2
-    var idadeDiv = idade / 2
-
-    println("Idade total: $idade")
-    println("Idade somada: $idadeSum")
-    println("Idade subitraida: $idadeSub")
-    println("Idade Multiplicada: $idadeMult")
-    println("Idade Dividida: $idadeDiv")
-
-    var resto = idade % 2
-
-    println("Resto da divisao: $resto")
-
-    // Outras formas de soma variaveis
-    println(idade++)
-    println(idade)
-    println(++idade)
-
 }
 
 
