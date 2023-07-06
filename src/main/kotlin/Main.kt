8253e24dd1e1055263cadd579f9147c3a5e779ef
Rafael Santos <rafael.santos@sysdata.com.br>
2023-07-06 18:04:44 -0300

    Funções para manipulção de string

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,35 @@
 fun main() {
-    println("Soma A + B e igual a: ${soma(10,20)}")
-}
+    val str="Coding in Kotlin"
 
-fun soma(A:Int,B:Int) = A + B
+    println(str)
+
+    //Tamanho numerico da string
+    println(str.length)
+
+    //Acessando possição expecifica da String
+    println("Acessando possição 10 da String: ${str[10]}")
+
+    //Verificando qual e o inicio da String
+    println(str.startsWith("c",true))
+    println(str.startsWith("a",false))
+
+    //Retorna um pedaço da string
+    println(str.substring(10,16))
+
+    //Troca parte da String
+    println(str.replace("Kotlin","Java"))
+
+    //Deixa a string toda em caixa baixa
+    println(str.lowercase())
+
+    //Deixa a string toda em caixa alta
+    println(str.uppercase())
+
+    //Remove espaçoes em branco do inicio da string
+    println("                123445".trim())
+
+
+
+}
 
 
