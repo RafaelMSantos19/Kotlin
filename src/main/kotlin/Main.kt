e1fb79038af549688acbb051d9abb382d924fb4c
Rafael Santos <rafael.santos@sysdata.com.br>
2023-07-06 17:39:22 -0300

    Usando funções

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,13 @@
 fun main() {
 
+    olamundo()
+    println("Soma A + B e igual a: ${soma(10,20)}")
 }
 
+fun soma(A:Int,B:Int): Int{
+    return A + B
+}
 
+fun olamundo(){
+    println("ola mundo")
+}
